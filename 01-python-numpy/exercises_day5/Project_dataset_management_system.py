a97a10ec96dbbc4e0d1033fc6429375e8e6896dd
AJMAL S <ajmal6sr@gmail.com>
2026-09-17 12:08:07 +0530

    feat: Dataset Management -  search, delete, and CSV export functionality

--- 01-python-numpy/exercises_day5/Project_dataset_management_system.py
+++ 01-python-numpy/exercises_day5/Project_dataset_management_system.py
@@ -3,6 +3,7 @@
 
 import json
 import os
+import csv
 
 JSON_File = "dataset.json"
 CSV_File = "dataset.csv"
@@ -81,15 +82,85 @@ class Dataset_Manager:
             print(f"Answer : {record["answer"]}")
             print(f"Category : {record["category"]}")
             
+    def search(self):
+        keyword=input("Enter the keyword to search :").strip().lower()
+        if not keyword:
+            print("Please enter a valid keyword")
+            return
+        flag=False
+        for record in self.dataset:
+            if  keyword in record["question"].lower() or keyword in record["answer"].lower() or keyword in record["category"].lower():
+                print("\n === Serach successfully complete ===\n")
+                print(f"ID {record["id"]}")
+                print(f"Question : {record["question"]}")
+                print(f"Answer : {record["answer"]}")
+                print(f"Category : {record["category"]}")
+                flag = True
+        if not flag:
+            print("No data found")
+    
+    def delete(self):
+        print("\nDelete Record")
+        try:
+            delete_id = int(input("Enter the record id :"))
+        except ValueError:
+            print("\nRecord id should be a number")
+            return
+        for record in self.dataset:
+            if delete_id==record["id"]:
+                print(f"The question is {record["question"]}")
+
+                print('\nAre you sure you want to delete this? ')
+                confirm=input("Y/N ? :").lower()
+                try:
+                    if confirm=="y":
+                        self.dataset.remove(record)
+                        print("Successfully deleted")
+                        self.save_data()
+                        return
+                    elif confirm=="n":
+                        print("Delete Cancelled")
+                        return
+                except ValueError:
+                    print("Please enter Y or N..")
+                    return
+            
+        print("Please enter a valid record id")
+        
+    def exp_csv(self):
+        print("CSV Exporting")
+        if not self.dataset:
+            print("No Records Found")
+            return
+        
+        try:
+            with open("CSV_File_EXP","w") as file:
+                        fieldname = ["id","question","answer","category"]
+                        writer= csv.DictWriter(file,fieldnames=fieldname)
+                        writer.writeheader()
+                        writer.writerows(self.dataset)
+                        print("Dataset Export Successfully")
+                        
+        except OSError :
+            print("Error unable to export CSV file")
+            
+            
+                
+        
+            
              
         
     
         
         
 ob=Dataset_Manager()
-ob.Add_Data()
+# ob.Add_Data()
 # ob.load_data()
-ob.view_records()
+# ob.view_records()
+# ob.search()
+# ob.delete()
+ob.exp_csv()
+
 
         
         
